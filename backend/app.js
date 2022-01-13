b33ee290889a185aefa3759a39993d4080a73d87
Gil Cohen <gilco77@gmail.com>
2022-01-13 13:18:36 +0200

    make it better

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const path = require('path');
+const dotenv = require('dotenv');
 const { requireAuth, checkUser } = require('./middlewares/authMiddleware');
 const authRoutes = require('./routes/authRoute');
 const folderRoutes = require('./routes/foldersRoute')
-
-const path = require('path');
-const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
 const app = express();
@@ -15,24 +14,33 @@ connectDB();
 app.use(express.json()); // exept jaon data from user
 app.use(cookieParser());
 
-/* --- set statics --- */
-app.set('view engine', 'ejs');
-const publicPath = path.join(__dirname, "../frontend/public")
-app.use(express.static(publicPath));
-const templatesPath = path.join(__dirname, "../frontend/views")
-app.set("views", templatesPath);
-
-
 /*--- routes --- */
 app.get('*', checkUser);
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
 
 app.use('/api/myfolders', folderRoutes);
 
 app.use("/api/users", authRoutes);
+
+//----deployment----------
+__dirname = path.resolve();
+if(process.env.NODE_ENV === 'production')
+{
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+else
+{
+  app.get('/', (req, res) => {
+    res.send('api is runing');
+  })
+}
+
+
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
